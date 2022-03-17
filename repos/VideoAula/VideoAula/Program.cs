d6187c582731462cb856817fb97cc2accf7abfb9
cesarmenchao <allacazan12@gmail.com>
2022-03-17 17:16:58 -0300

    feito ate o switch

--- repos/VideoAula/VideoAula/Program.cs
+++ repos/VideoAula/VideoAula/Program.cs
@@ -10,12 +10,16 @@ namespace VideoAula
     {
         static void Main(string[] args)
         {
+            // esta puxando a funçao soma la em baixo
             /*int conta = Soma(20, 30);
             int resultado = Soma(99, 584);
 
             Console.WriteLine(conta);
             Console.WriteLine(resultado);
             Console.ReadLine();*/
+
+            //array simples 
+            /*
             string[] lista = new string [5] {
                     "robson",//0
                     "ronaldo",//1
@@ -27,6 +31,29 @@ namespace VideoAula
 
             Console.WriteLine(lista[3]);
 
+            Console.ReadLine();
+            */
+
+            //switch basico usando string
+            string cor = "cinza";
+            switch (cor)
+            {
+                case "vermelho":
+                    Console.WriteLine("Sua cor é vermelha");
+                    break;
+
+                case "azul":
+                    Console.WriteLine("Sua cor é azul");
+                    break ;
+                case "roxo":
+                    Console.WriteLine("Sua cor é roxa");
+                    break;
+                default:
+                    Console.WriteLine("Sua cor nao se encadra");
+                    break;
+
+                    
+            }
             Console.ReadLine();
         }
 
