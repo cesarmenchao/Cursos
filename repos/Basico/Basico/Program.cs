f64b26f8d4d8c679199af5a7c67b4a46df97256f
cesarmenchao <allacazan12@gmail.com>
2022-03-17 16:49:31 -0300

    primeiro

---
+++ repos/Basico/Basico/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+            int anoNascimento = 1997;
+            string meuNome = "Cesar";
+            float altura = 1.85f;
+
+                
+
+                    
+                    Console.WriteLine(meuNome);
+                    Console.WriteLine(anoNascimento);
+                    Console.WriteLine(altura);
+
+            altura = 1.95f;
+
+                    Console.WriteLine(altura);
+            */
+
+            //variaveis declaradas como var identificam automaticamente o tipo da variavel
+            /*var testeVar = 123;  
+            var testeVar2 = "cesar";
+
+            Console.WriteLine(testeVar);
+
+            testeVar = 221;
+
+            Console.WriteLine(testeVar);
+            */
+
+            Console.WriteLine("informe o nome do  Usuario");
+            string usuario = Console.ReadLine();
+            Console.Write("O nome do usuario é: ");
+            Console.WriteLine(usuario);
+
+
+
+
+            Console.ReadLine();
+            
+
+        }
+    }
+}
---
+++ repos/VideoAula/VideoAula/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int conta = Soma(20, 30);
+            int resultado = Soma(99, 584);
+
+            Console.WriteLine(conta);
+            Console.WriteLine(resultado);
+            Console.ReadLine();*/
+            string[] lista = new string [5] {
+                    "robson",//0
+                    "ronaldo",//1
+                    "clovis",//2
+                    "cesar",//3
+                    "mari",//4
+
+            };
+
+            Console.WriteLine(lista[3]);
+
+            Console.ReadLine();
+        }
+
+         
+        //static int Soma(int a, int b) {
+
+          //  int resultadoSoma = a + b;
+            //return resultadoSoma;
+              //  }
+      
+    }
+}
