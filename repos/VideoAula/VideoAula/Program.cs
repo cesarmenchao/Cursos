270408f59ad29db7996a0908ba334d233c6f0863
cesarmenchao <allacazan12@gmail.com>
2022-03-18 17:32:35 -0300

    enum + switch
    criando telinha com menu no console!!

--- repos/VideoAula/VideoAula/Program.cs
+++ repos/VideoAula/VideoAula/Program.cs
@@ -8,13 +8,14 @@ namespace VideoAula
 {
     internal class Program
     {
+        enum Opcao { Incluir = 1, Excluir, Alterar, Listar, Atualizar }
         //enum enumeraçao de valores
-        enum carro {palio, gol, fusca, variant }
+        //  enum carro {palio, gol, fusca, variant }
 
         static void Main(string[] args)
         {
-            carro carroJulio = carro.palio; //puxei o enum e o valor dele com o ponto
-            Console.WriteLine((int)carroJulio); // com o parenteses informei parar puxar o valor inteiro da variavel carroJulio, convertendo de string para o valor da enumeraçao do enum.
+            //       carro carroJulio = carro.palio; //puxei o enum e o valor dele com o ponto
+            //       Console.WriteLine((int)carroJulio); // com o parenteses informei parar puxar o valor inteiro da variavel carroJulio, convertendo de string para o valor da enumeraçao do enum.
 
             // esta puxando a funçao soma la em baixo
             /*int conta = Soma(20, 30);
@@ -41,27 +42,49 @@ namespace VideoAula
             */
 
             //switch basico usando string
-          /*  string cor = "cinza";
-            switch (cor)
+            /*  string cor = "cinza";
+              switch (cor)
+              {
+                  case "vermelho":
+                      Console.WriteLine("Sua cor é vermelha");
+                      break;
+
+                  case "azul":
+                      Console.WriteLine("Sua cor é azul");
+                      break ;
+                  case "roxo":
+                      Console.WriteLine("Sua cor é roxa");
+                      break;
+                  default:
+                      Console.WriteLine("Sua cor nao se encadra");
+                      break;
+
+
+              }*/
+
+            Console.WriteLine("Selecione um das opções abaixo");
+            Console.WriteLine("1-Incluir\n2-Excluir\n3-Alterar\n4-Listar\n5-Atualizar");
+                int index = int.Parse(Console.ReadLine());
+                 Opcao opcaoSelecionada = (Opcao)index;
+             switch (opcaoSelecionada)
             {
-                case "vermelho":
-                    Console.WriteLine("Sua cor é vermelha");
+                case Opcao.Incluir:
+                    Console.WriteLine("Você quer incluir algo!!");
+                    break;
+                case Opcao.Excluir:
+                    Console.WriteLine("Oque deseja excluir?");
+                    break;
+                case Opcao.Alterar:
+                    Console.WriteLine("Oque deseja incluir?");
                     break;
 
-                case "azul":
-                    Console.WriteLine("Sua cor é azul");
-                    break ;
-                case "roxo":
-                    Console.WriteLine("Sua cor é roxa");
-                    break;
                 default:
-                    Console.WriteLine("Sua cor nao se encadra");
+                    Console.WriteLine("Opçao nao encontrada!!");
                     break;
 
-                    
-            }*/
-                //enum
 
+            }
+            
 
 
 
