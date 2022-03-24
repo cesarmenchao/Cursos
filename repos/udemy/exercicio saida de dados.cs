0e379e7d6a4a4ae0cc7e1d38939be394311d59de
cesarmenchao <allacazan12@gmail.com>
2022-03-24 12:05:32 -0300

    EXERCICIO

---
+++ repos/udemy/exercicio saida de dados.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        string produto1 = "Computador";
+        string produto2 = "Mesa de escritório";
+
+        byte idade = 30;
+        int codigo = 5290;
+        char genero = 'M';
+
+        double preco1 = 2100.0;
+        double preco2 = 650.50;
+        double medida = 53.234567;
+
+        Console.WriteLine("Produtos:");
+        Console.WriteLine(produto1 + ", cujo o preço é " preco1);
+        Console.WriteLine(produto2 + ", cujo p preço é " preco2);
+
+
+        Console.WriteLine("Medida com oito casas decimais: " medida);
+        Console.WriteLine("Arredondado (três casas decimais )"{ medida:f3});
+
+
+    }
+}
--- repos/udemy/udemy/Program.cs
+++ repos/udemy/udemy/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Globalization;
 
 namespace udemy
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             bool completo = false;
             char genero = 'F';
             char letra = '\u0041';
@@ -30,6 +31,27 @@ namespace udemy
             Console.WriteLine(nome);
             Console.WriteLine(obj1);
             Console.WriteLine(obj2);
+            */
+
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine(produto1 + ", cujo o preço é " + preco1);
+            Console.WriteLine(produto2 + ", cujo p preço é " + preco2);
+
+
+            Console.WriteLine("Medida com oito casas decimais: " + medida);
+            Console.WriteLine("Arredondado (três casas decimais )" + medida.ToString("f3"));
+            Console.WriteLine("Separador decimal invariant " + medida.ToString("f3", CultureInfo.InvariantCulture));
         }
     }
 }
