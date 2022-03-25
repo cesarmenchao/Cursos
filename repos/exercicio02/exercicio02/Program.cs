66134405d6cc898147aa9d822fca74fa5ee90917
cesarmenchao <allacazan12@gmail.com>
2022-03-25 16:49:34 -0300

    exercicio 03

---
+++ repos/exercicio02/exercicio02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                     Calculadora de Salario                  ");
+            Console.WriteLine("Entre com o numnero do funcionario:");
+            int numFuncionario = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe as horas trabalhadas:");
+            int hrs = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o valor por hora trabalhada:");
+            double hrTrabalho = double.Parse(Console.ReadLine());
+
+            double salario = hrTrabalho * hrs ;
+
+            Console.WriteLine("O salario do funcionario este mês é de :" + salario.ToString("f2"));
+        }
+    }
+}
