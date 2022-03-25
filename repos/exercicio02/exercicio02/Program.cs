8f883c036cf0d9ab34ce32098da8b25a5d36208d
cesarmenchao <allacazan12@gmail.com>
2022-03-25 17:20:43 -0300

    mais um exercicio 25/03/22

--- repos/exercicio02/exercicio02/Program.cs
+++ repos/exercicio02/exercicio02/Program.cs
@@ -14,9 +14,22 @@ namespace exercicio02
             Console.WriteLine("Informe o valor por hora trabalhada:");
             double hrTrabalho = double.Parse(Console.ReadLine());
 
-            double salario = hrTrabalho * hrs ;
+            double salario = hrTrabalho * hrs;
 
             Console.WriteLine("O salario do funcionario este mês é de :" + salario.ToString("f2"));
+
+            /*int numero, horas;
+            double valorHora, salario;
+
+            numero = int.Parse(Console.ReadLine());
+            horas = int.Parse(Console.ReadLine());
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = valorHora * horas;
+
+            Console.WriteLine("NUMBER = " + numero);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+            */
         }
     }
 }
