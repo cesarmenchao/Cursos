7d7709b7f249e270d697c76567955da79aaba45d
cesarmenchao <allacazan12@gmail.com>
2022-03-25 14:49:03 -0300

    25/03

---
+++ repos/ExercicioEntradaDeDados/ExercicioEntradaDeDados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+namespace ExercicioEntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string meuNome = Console.ReadLine();
+            Console.WriteLine(meuNome);
+
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine(quartos);
+
+
+            Console.WriteLine("Entre com o preço dos produtos");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine(preco.ToString(CultureInfo.InvariantCulture));
+
+
+            Console.WriteLine("Entre com seu ultimo nome, idade, altura.(na mesma linha)");
+            string[] linha = Console.ReadLine().Split(' ');
+            string nome = (linha[0]);
+            int idade = int.Parse(linha[1]);
+            double altura = double.Parse(linha[2],CultureInfo.InvariantCulture);
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString(CultureInfo.InvariantCulture));
+
+
+
+
+
+
+        }
+    }
+}
---
+++ repos/exercicio01/exercicio01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse( Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            //int num4 = int.Parse(Console.ReadLine());
+
+            int soma = (num1 * num2 - num3 * num4 );
+
+            Console.WriteLine("O resultado das entradas correspomde a : " + soma);
+            Console.WriteLine();
+
+        }
+    }
+}
